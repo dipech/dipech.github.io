170699ac6558aabea0f4978b898c34d01e5775c4
DiPech <dmitry.pechkovsky@gmail.com>
2021-04-04 16:38:43 +0300

    - A lot of changes.

--- src/js/components/partials/footer.vue
+++ src/js/components/partials/footer.vue
@@ -1,11 +1,12 @@
 <template>
     <footer class="bg-light mt-3 mb-3 p-4">
         <div class="row">
-            <div class="col-12 col-md-6 text-center text-sm-left">
+            <div class="col-12 col-sm-6 col-md-6 text-center text-sm-left">
                 <a :href="siteUrl">{{ domain }}</a>, {{ year }}
             </div>
-            <div class="col-12 col-md-6 text-center text-sm-right">
-                <div class="font-italic">
+            <div class="col-12 col-sm-6 col-md-6 text-center text-sm-right">
+                <div class="font-italic not-selectable not-focusable"
+                     @click="handleClick">
                     All rights are lost ©
                 </div>
             </div>
@@ -19,14 +20,30 @@
 
     @Component
     export default class FooterComponent extends Vue {
+
+        counter: number = 1;
+
         get domain(): string {
             return window.location.hostname;
         }
+
         get siteUrl(): string {
             return window.location.protocol + "//" + this.domain;
         }
+
         get year(): number {
             return new Date().getFullYear();
+        }
+
+        get isAdminMode() {
+            return this.$store.state.isAdminMode;
+        }
+
+        handleClick() {
+            if (this.counter++ >= 10 && !this.isAdminMode) {
+                console.log("Admin mode has been activated!");
+                this.$store.commit("setAdminMode", true);
+            }
         }
     }
 </script>
